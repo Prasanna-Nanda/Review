ac73843a424fff2dda8430972874dd9ab1ffbe9a
Prasanna-Nanda <prasannanandakumar92@gmail.com>
2021-11-06 15:33:42 +0530

    first commit

---
+++ integer.java
@@ -0,0 +1,12 @@
+int x;
+int n =0; 
+double avg;
+System.out.println("Enter 5 number" );
+for (int i=0; i<5: i++){
+Scanner tl = new Scanner(System.in);
+
+n=next.int();
+x +=n;
+}
+
+System.out.println(x);
